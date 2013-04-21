4cfa4a2e95f00350f8c6231fca98f1b1e152b418
Unknown <joe2010xtmf@163.com>
2013-04-20 22:19:12 -0400

    03.9.md

---
+++ eBook/examples/chapter_3/CandGo/c2.go
@@ -0,0 +1,13 @@
+// c2.go
+package print
+
+// #include <stdio.h>
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+
+func Print(s string) {
+   cs := C.CString(s)
+   defer C.free(unsafe.Pointer(cs))
+   C.fputs(cs, (*C.FILE)(C.stdout))
+}
