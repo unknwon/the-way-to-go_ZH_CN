111e409bb73978951466f80f1d3a04d11d89142b
gostar <lihexingstar@163.com>
2015-11-26 16:28:54 +0800

    Update gob2.go
    为0创建不成功

--- eBook/examples/chapter_12/gob2.go
+++ eBook/examples/chapter_12/gob2.go
@@ -28,7 +28,7 @@ func main() {
 	vc := VCard{"Jan", "Kersschot", []*Address{pa,wa}, "none"}
 	// fmt.Printf("%v: \n", vc) // {Jan Kersschot [0x126d2b80 0x126d2be0] none}: 
 	// using an encoder:
-	file, _ := os.OpenFile("vcard.gob", os.O_CREATE|os.O_WRONLY, 0)
+	file, _ := os.OpenFile("vcard.gob", os.O_CREATE|os.O_WRONLY, 0666)
 	defer file.Close()
 	enc := gob.NewEncoder(file)
 	err := enc.Encode(vc)
