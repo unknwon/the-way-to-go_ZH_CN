57df7df3975651fa0b93ccbb20d2af1b25b363b5
glight2000 <173959153@qq.com>
2015-12-09 23:17:02 +0800

    Create hello_world_webserver.go

---
+++ eBook/examples/chapter_15/hello_world_webserver.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Inside HelloServer handler")
+	fmt.Fprintf(w, "Hello,"+req.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err.Error())
+	}
+}
