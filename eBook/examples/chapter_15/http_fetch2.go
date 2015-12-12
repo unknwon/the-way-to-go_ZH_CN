0f1e597289be28ebe58c6ded4533f242fc1bfaa3
glight2000 <173959153@qq.com>
2015-12-12 21:22:19 +0800

    Create http_fetch2.go

---
+++ eBook/examples/chapter_15/http_fetch2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter the url...")
+	url, err := inputReader.ReadString('\n')
+	url = strings.TrimSuffix(url, "\r\n")
+	checkError(err)
+	res, err := http.Get(url)
+	checkError(err)
+	data, err := ioutil.ReadAll(res.Body)
+	checkError(err)
+	fmt.Printf("Got: %q", string(data))
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalf("Get : %v", err)
+	}
+}
