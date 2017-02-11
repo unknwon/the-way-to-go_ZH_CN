72c38397349dfcb739d979cbb09e8de48250e6cb
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-11 12:24:19 +0800

    fix: coding style and file format for chapter 6.

--- eBook/exercises/chapter_6/fibonacci2.go
+++ eBook/exercises/chapter_6/fibonacci2.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	pos := 4
-	result, pos := fibonacci(pos) 
+	result, pos := fibonacci(pos)
 	fmt.Printf("the %d-th fibonacci number is: %d\n", pos, result)
 	pos = 10
-	result, pos = fibonacci(pos) 
+	result, pos = fibonacci(pos)
 	fmt.Printf("the %d-th fibonacci number is: %d\n", pos, result)
 }
 
@@ -15,9 +15,9 @@ func fibonacci(n int) (val, pos int) {
 	if n <= 1 {
 		val = 1
 	} else {
-		v1, _ := fibonacci(n-1)
-		v2, _ := fibonacci(n-2)
-		val =  v1 + v2
+		v1, _ := fibonacci(n - 1)
+		v2, _ := fibonacci(n - 2)
+		val = v1 + v2
 	}
 	pos = n
 	return
--- eBook/exercises/chapter_6/mult_returnval.go
+++ eBook/exercises/chapter_6/mult_returnval.go
@@ -2,23 +2,24 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func SumProductDiff(i, j int) (int, int, int) {
-    return i+j, i*j, i-j
+	return i + j, i * j, i - j
 }
 
 func SumProductDiffN(i, j int) (s int, p int, d int) {
 	s, p, d = i+j, i*j, i-j
-    return 
+	return
 }
 
-func main() { 
-    sum, prod, diff := SumProductDiff(3,4)
-    fmt.Println("Sum:", sum, "| Product:",prod, "| Diff:", diff)
- 	sum, prod, diff = SumProductDiffN(3,4)
-    fmt.Println("Sum:", sum, "| Product:",prod, "| Diff:", diff)
+func main() {
+	sum, prod, diff := SumProductDiff(3, 4)
+	fmt.Println("Sum:", sum, "| Product:", prod, "| Diff:", diff)
+	sum, prod, diff = SumProductDiffN(3, 4)
+	fmt.Println("Sum:", sum, "| Product:", prod, "| Diff:", diff)
 }
+
 // Sum: 7 | Product: 12 | Diff: -1
 // Sum: 7 | Product: 12 | Diff: -1
