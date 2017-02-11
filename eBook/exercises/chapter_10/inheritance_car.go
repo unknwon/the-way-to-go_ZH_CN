ca79293078768245cd5fcde883a62c124972cef7
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-11 12:28:43 +0800

    fix: coding style and file format for chapter 10.

--- eBook/exercises/chapter_10/inheritance_car.go
+++ eBook/exercises/chapter_10/inheritance_car.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Engine interface {
-    Start()
-    Stop()
+	Start()
+	Stop()
 }
 
 type Car struct {
@@ -17,16 +17,16 @@ type Car struct {
 
 // define a behavior for Car
 func (car Car) numberOfWheels() int {
-    return car.wheelCount
+	return car.wheelCount
 }
 
 type Mercedes struct {
-    Car //anonymous field Car
+	Car //anonymous field Car
 }
 
 // a behavior only available for the Mercedes
 func (m *Mercedes) sayHiToMerkel() {
-    fmt.Println("Hi Angela!")
+	fmt.Println("Hi Angela!")
 }
 
 func (c *Car) Start() {
@@ -38,19 +38,20 @@ func (c *Car) Stop() {
 }
 
 func (c *Car) GoToWorkIn() {
-    // get in car 
-    c.Start();
-    // drive to work 
-    c.Stop();
-    // get out of car 
+	// get in car
+	c.Start()
+	// drive to work
+	c.Stop()
+	// get out of car
 }
 
 func main() {
 	m := Mercedes{Car{4, nil}}
- 	fmt.Println("A Mercedes has this many wheels: ", m.numberOfWheels())
+	fmt.Println("A Mercedes has this many wheels: ", m.numberOfWheels())
 	m.GoToWorkIn()
 	m.sayHiToMerkel()
 }
+
 /* Output:
 A Mercedes has this many wheels:  4
 Car is started
