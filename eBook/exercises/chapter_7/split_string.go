e6d601d3a1ab98b5ec12517ca4899b32edb2d1aa
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-11 12:26:05 +0800

    fix: coding style and file format for chapter 7.

--- eBook/exercises/chapter_7/split_string.go
+++ eBook/exercises/chapter_7/split_string.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	str := "Google"
-	for i:=0; i <= len(str); i++ {
+	for i := 0; i <= len(str); i++ {
 		a, b := Split(str, i)
 		fmt.Printf("The string %s split at position %d is: %s / %s\n", str, i, a, b)
 	}
-	
+
 }
 
 func Split(s string, pos int) (string, string) {
