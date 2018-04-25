6007f062436542aee96ce53f33c792b23e501fbb
lizebang <lizebang1998@gmail.com>
2018-04-25 08:21:38 +0800

    #419 #425 #272

--- eBook/examples/chapter_4/goos.go
+++ eBook/examples/chapter_4/goos.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 )
 
 func main() {
-	var goos string = os.Getenv("GOOS")
+	var goos string = runtime.GOOS
 	fmt.Printf("The operating system is: %s\n", goos)
 	path := os.Getenv("PATH")
 	fmt.Printf("Path is %s\n", path)
