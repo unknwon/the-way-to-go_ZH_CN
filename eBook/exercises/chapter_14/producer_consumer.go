bb225a358ab68319cd004647b68e3729149bf19c
Respawnz <47511522+Respawnz@users.noreply.github.com>
2019-04-23 14:19:33 +0800

    提供了超纲知识所在的章节链接

--- eBook/exercises/chapter_14/producer_consumer.go
+++ eBook/exercises/chapter_14/producer_consumer.go
@@ -4,7 +4,9 @@ package main
 import "fmt"
 
 // integer producer:
-func numGen(start, count int, out chan<- int) {
+//译者注：numGen函数中第三个参数所指定的 chan<- 类型的相关知识在 14.2.11 “通道的方向”，链接如下：
+//https://github.com/Unknwon/the-way-to-go_ZH_CN/blob/master/eBook/14.2.md#14211-%E9%80%9A%E9%81%93%E7%9A%84%E6%96%B9%E5%90%91
+func numGen(start, count int, out chan<- int) { 
 	for i := 0; i < count; i++ {
 		out <- start
 		start = start + count
