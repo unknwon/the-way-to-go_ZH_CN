dc86a917a6c424d360d8e39735976080536dce86
Luke Zhou <60960532+zhouzilong2020@users.noreply.github.com>
2021-10-23 11:49:46 +0800

    fix spelling error and some improper demo (#807)
    Co-authored-by: zhouzilong.luke <zhouzilong.luke@bytedance.com>
    Co-authored-by: Joe Chen <jc@unknwon.io>

--- eBook/examples/chapter_14/channel_block.go
+++ eBook/examples/chapter_14/channel_block.go
@@ -1,15 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+var cnt = 0
 
 func main() {
 	ch1 := make(chan int)
 	go pump(ch1)       // pump hangs
 	fmt.Println(<-ch1) // prints only 0
+
+	time.Sleep(time.Second)
+	fmt.Println(cnt) // prints 1
+
 }
 
 func pump(ch chan int) {
 	for i := 0; ; i++ {
-		ch <- i
+		ch <- i // the channel will be block due to lack of consumer
+		cnt++   // this code will only execute once
 	}
 }
